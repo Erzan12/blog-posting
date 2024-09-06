396dbcdc3117df25c0ea6e86b83f15bd26e30a4b
erzan <yoboijanjan30@gmai.com>
2024-09-06 16:19:44 +0800

    delete blog post functionality

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Handle the listing of all the blog post
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
+// Handle the creation of blogs
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+// Handle the storing of blogs into the database
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
+// Handle the deletion of a blog post
+Route::delete('/post/{post}',[PostController::class,'destroy'])->name('posts.destroy');
 
